724e6e1e143af93e9f93fec8f54e42ba51aca31e
Chadnaut <63690461+Chadnaut@users.noreply.github.com>
2023-09-22 21:13:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,124 @@
+# WaraWara Plaza XML Generator v0.1
+
+**USE AT YOUR OWN RISK - EDITING NAND CAN BRICK YOUR SYSTEM - READ ALL INSTRUCTIONS & GBATEMP THREAD FIRST**
+
+## Usage
+
+Requires:
+- [Python](https://www.python.org/downloads/)
+- [Pillow](https://pillow.readthedocs.io/en/latest/installation.html#basic-installation)
+- May need to run as administrator when installing
+- May need to replace `python3` with `python`
+
+```pwsh
+# output to terminal
+python plaza.py
+
+# default files config.ini 1stNUP.xml
+python plaza.py -o
+
+# specify files
+python plaza.py -i config.ini -o output.xml
+
+# force output overwrite
+python plaza.py -i config.ini -o output.xml -f
+```
+
+## Package Contents
+
+- `icons/` - contains topic icons
+- `painting/` - contains post paintings
+- `base.ini` - config to produce XML nearly identical to the base XML
+- `config.ini` - config example, edit as necessary
+- `plaza.py` - this is where the magic happens!
+- `README.md` - you are here
+
+## Configuration
+
+### Topics
+
+```ini
+[topic.0]
+name = YourTitleHere
+title_id = 0000000000000001
+icon = images/icon.tga
+is_recommended = 0
+posts = "
+    My Post Content 1
+    My Post Content 2
+    My Post Content 3
+    "
+```
+
+Requires `[topic.0]` through `[topic.9]`
+- `name` may contain specials [', ", #, /]
+- `title_id` may be hex or dec format (converted to dec for XML)
+- `community_id` (optional) community id
+- `icon` (optional) path to image (any format), or pre-formatted base64 tga (not recommended)
+- `is_recommended` (optional) set to 1 to display a highlighted frame
+- `reply_count` (optional) comma delimited count for post replies (not recommended)
+- `posts` each line contains a separate message
+
+If `icon` is omitted the `icons/` folder will be scanned for [png, tga] files matching the `title_id` [dec, hex] - the XML will not build if no image is found.
+
+The `community_id` and `reply_count` are used to generate XML similar to the base XML, and are not required for normal use.
+
+Posts should be around `60` characters, anything over usually gets cropped.
+
+### People
+
+```ini
+[people]
+person = "
+    AwAAQJkt7s+FJgPA1sdZ9YphpRp2gAAAgl... ScreenName1
+    AwAAME4NX6Ef9jiRlZ8zCEDSiojFNQAAqG... ScreenName2
+    "
+```
+
+- `person` each lines contains separate `mii` code plus screen name separate by a space (no spaces allowed in the name)
+
+People are selected for topic posts in the order they appear. The mii code is not editable as it is protected by a checksum. Note that screen names are not actually used - there must be a separate list somewhere...
+
+### Defaults
+
+```ini
+[result.default]
+...
+
+[topic.default]
+...
+
+[post.default]
+...
+
+[painting.default]
+...
+```
+
+These sections contain default values for the XML section - some are *essential*, but many may be omitted and the XML will still work fine (compare `base.ini` to the trimmed down `config.ini`).
+
+- `modified_at` if empty will use the current datetime
+- `created_at` if empty will use the current datetime
+
+## Images
+
+Topic icon images may be copied from the WiiU directly, for example:
+- `/storage_usb/usr/title/0050000/********/meta/iconTex.tga`
+
+All images are run through the image library to resize & convert them as necessary, so in theory *any* image size and format can be used.
+
+## Known Limitations
+
+- WiiU requires XML to have full closing `<tags></tags>` - short-hand `<tags />` are not allowed
+- Improperly formatted image content prevents the parent element from being recognised:
+  - For Mii pictures this means the Mii simply will not appear
+  - For Topic icons this means the Plaza will **freeze**
+
+If the system freezes hard-reboot into the homebrew launcher (hold X while powering on) and replace the XML with the last good version.
+
+## Credits
+
+Thanks to MikaDubbz & CaramelKat for making this possible
+
+- https://gbatemp.net/threads/i-permanently-gave-wara-wara-plaza-life-again-injected-it-full-of-my-personality-and-you-can-too.562257
+- https://github.com/CaramelKat/WaraWaraPlazaBase64Encoder
---
+++ plaza.py
@@ -0,0 +1,183 @@
+import io, os, sys, argparse, html, re, configparser, zlib, base64, xml.etree.cElementTree as ET
+from PIL import Image
+from datetime import datetime
+
+# --------------------------------------------------
+# Constants
+
+img_path = 'icons/'
+max_path_len = 10000
+topic_image_size = (128, 128)
+post_image_size = (320, 120)
+
+# --------------------------------------------------
+# Arguments
+
+parser = argparse.ArgumentParser(description = 'WaraWara Plaza XML Generator v0.1')
+parser.add_argument('-i', '--input', help = 'configuration file', default = 'config.ini')
+parser.add_argument('-o', '--output', help = 'output file', nargs = '?', default = None, const = '1stNUP.xml')
+parser.add_argument('-f', '--force', help = 'force overwrite', action = 'store_true')
+args = parser.parse_args()
+
+if not os.path.isfile(args.input):
+    sys.exit(f'{args.input} not found')
+
+if args.output and os.path.isfile(args.output) and not args.force:
+    if input(f'{args.output} exists, overwrite (Y/N) ? ').upper() != 'Y':
+        sys.exit('Aborted')
+
+# --------------------------------------------------
+# Helpers
+
+def hex_to_dec(v):
+    return str(int(v, 16)) if v[0] == '0' else v
+
+def dec_to_hex(v):
+    return hex(int(v)).replace('0x', '00') if re.search('^\d+$', v) else '0'
+
+def strip_quotes(v):
+    return v.strip('"\' \t\r\n')
+
+def split_str(v):
+    return strip_quotes(v).split('\n')
+
+def now_if_empty(v):
+    return v or datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def image_paths(v):
+    v1 = strip_quotes(v)
+    v2 = hex_to_dec(v1)
+    v3 = dec_to_hex(v2)
+    return [v1, f'{img_path}{v1}.tga', f'{img_path}{v1}.png', f'{img_path}{v2}.tga', f'{img_path}{v2}.png', f'{img_path}{v3}.tga', f'{img_path}{v3}.png']
+
+def valid_image_path(v):
+    paths = list(filter(os.path.isfile, image_paths(v)))
+    if len(paths) == 0:
+        sys.exit(f'Invalid image: {v}')
+    return paths[0]
+
+def image_bytes(path, size):
+    b = io.BytesIO()
+    with Image.open(path) as im:
+        im = im.convert('RGBA')
+        im = im.resize(size)
+        im.save(b, format = 'TGA', compression = None)
+    return b.getvalue()
+
+def encode_image(value, size):
+    if len(value) > max_path_len: return value
+    data = image_bytes(valid_image_path(value), size)
+    return base64.b64encode(zlib.compress(data, 6)).decode('ascii')
+
+def topic_image(value):
+    return encode_image(value, topic_image_size)
+
+def post_image(value):
+    return encode_image(value, post_image_size)
+
+def default_elements(target, source):
+    for (key, val) in source.items(): ET.SubElement(target, key).text = val
+    return target
+
+def optional_text(element, sub, text):
+    e = element.find(sub)
+    if e != None: e.text = text
+
+# --------------------------------------------------
+# Classes
+
+class Topic:
+    def __init__(self, data):
+        self.name = data.get('name')
+        self.title_id = hex_to_dec(data.get('title_id'))
+        self.community_id = data.get('community_id', '0')
+        self.is_recommended = data.get('is_recommended', '0')
+        self.icon = topic_image(data.get('icon', self.title_id))
+        self.reply_count = data.get('reply_count', '0').split(',')
+        self.posts = [html.unescape(item) for item in split_str(data.get('posts'))]
+
+class Person:
+    def __init__(self, mii, screen_name = ''):
+        self.mii = mii
+        self.screen_name = screen_name
+
+# --------------------------------------------------
+# Parse config
+
+config = configparser.ConfigParser()
+config.read(args.input)
+
+result_default = config['result.default']
+topic_default = config['topic.default']
+post_default = config['post.default']
+painting_default = config['painting.default']
+
+topic_data = [Topic(config[f'topic.{i}']) for i in range(0, 10)]
+people_data = [Person(*item.split(' ')) for item in split_str(config['people'].get('person'))]
+people_index = 0
+
+# --------------------------------------------------
+# XML Elements
+
+def next_person():
+    global people_index
+    person = people_data[people_index % len(people_data)]
+    people_index += 1
+    return person
+
+def post_painting(post, body):
+    has_image = os.path.isfile(body)
+    p = post.find('painting')
+    if has_image and p == None: p = ET.SubElement(post, 'painting')
+    if p != None: default_elements(p, painting_default)
+    if has_image:
+        post.find('body').text = ''
+        p.find('content').text = post_image(body)
+    return post
+
+def person_element(body, reply_count = 0):
+    person = ET.Element('person')
+    posts = ET.SubElement(person, 'posts')
+    post = ET.SubElement(posts, 'post')
+    default_elements(post, post_default)
+    data = next_person()
+    post.find('body').text = body
+    post.find('mii').text = data.mii
+    post.find('screen_name').text = data.screen_name
+    post.find('created_at').text = now_if_empty(post.find('created_at').text)
+    post_painting(post, body)
+    optional_text(post, 'reply_count', reply_count)
+    return person
+
+def topic_element(data: Topic):
+    topic = ET.Element('topic')
+    default_elements(topic, topic_default)
+    topic.find('icon').text = data.icon
+    topic.find('name').text = data.name
+    topic.find('title_id').text = data.title_id
+    topic.find('is_recommended').text = data.is_recommended
+    topic.find('modified_at').text = now_if_empty(topic.find('modified_at').text)
+    optional_text(topic, 'community_id', data.community_id)
+    p = topic.find('people')
+    for i, body in enumerate(data.posts): p.append(person_element(body, data.reply_count[i % len(data.reply_count)]))
+    return topic
+
+def result_element():
+    result = ET.Element('result')
+    default_elements(result, result_default)
+    topics = result.find('topics')
+    for data in topic_data: topics.append(topic_element(data))
+    return result
+
+# --------------------------------------------------
+# Output
+
+tree = ET.ElementTree(result_element())
+ET.indent(tree, space = '  ', level = 0)
+xml = '<?xml version="1.0" encoding="UTF-8"?>\n' + ET.tostring(tree.getroot(), encoding = 'utf-8', method = 'xml', short_empty_elements = False).decode('ascii')
+
+if args.output != None:
+    with open(args.output, 'w') as f: f.write(xml)
+    print(f'Generated {args.output} from {args.input}')
+else:
+    print(xml)
